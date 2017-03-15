476d46c84d24e3af239cc7645f9dfdee076320b3
Robso Duarte <robson.o.d@gmail.com>
2017-03-15 09:39:29 -0300

    add ionic refresh component

--- ionic-one-from-web-to-mobile/caffeinehit-list/www/js/controllers.js
+++ ionic-one-from-web-to-mobile/caffeinehit-list/www/js/controllers.js
@@ -2,4 +2,13 @@ var app = angular.module('caffeinehit.controllers', []);
 
 app.controller("YelpController", function ($scope, YelpService) {
 	$scope.yelp = YelpService;
+
+
+	$scope.doRefresh = function(){
+		if(!$scope.yelp.isLoading){
+			$scope.yelp.refresh().then(function(){
+				$scope.$broadcast('scroll.refreshComplete')
+			})
+		}
+	}
 });
